b8b3dab8ee4005c8f21ea1de3260aae523277f87
Shaneen Muhammad <mshaneen.sm@gmail.com>
2019-06-06 17:38:29 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -10,12 +10,4 @@ def countdown(int)
 end 
 
 def countdown_with_sleep(num)
-  num = 0 
-  while num > 0 
-    puts "#{num} SECOND(S)!"
-    sleep(1)
-    int -= 1
-  end 
-    return "happy new year!".upcase
-end 
-
+ 
