88ae3a80f0c5a28577ec8c1d399e05e8b024acb1
Shaneen Muhammad <mshaneen.sm@gmail.com>
2019-06-06 17:27:40 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,8 +1,8 @@
 #write your code here
 
-def countdown(int)
-  int = 1 
-  while int > 1 
+def countdown(number)
+  number = 1 
+  while number > 1 
     puts "#{int} SECOND(S)!"
     int -= 1
     return "happy new year !".upcase
